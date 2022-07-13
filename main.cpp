93605001772702ab98a43b91bf38503dffb1ee34
Fahid Shehzad <fahidsh@hotmail.com>
2022-07-13 05:34:57 +0000

    extreme schlankes verbindungscode

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 #define YIELD_TIME 100ms
 #define SLEEP_TIME 100ms
 #define SLEEP_TIME_BETWEEN_INIT_STEPS 200ms
-bool critical_failure = false;
 
 // Debugger
 #define SHOW_SERIAL_DEBUG_MESSAGES true
@@ -12,9 +11,8 @@ bool critical_failure = false;
 
 // WLANN
 #include "ESP8266Interface.h"
-ESP8266Interface
-    wifi(PC_12,
-         PD_2); // ANPASSEN: bitte die PINS for WLAN Module setzen (TX, RX)
+// ANPASSEN: bitte die PINS for WLAN Module setzen (TX, RX)
+ESP8266Interface wifi(PC_12, PD_2);
 
 // extra thread for mqtt
 Thread mqtt_thread;
@@ -26,18 +24,17 @@ SocketAddress socket_address;
 #include <MQTTClient.h>
 #include <MQTTNetwork.h>
 #include <MQTTmbed.h> // Countdown
-const std::string MQTT_SERVER =
-    "192.168.43.1"; // ANPASSEN: MQTT-Server domain oder IP
+// ANPASSEN: MQTT-Server domain oder IP
+const std::string MQTT_SERVER = "192.168.43.1";
 const unsigned int MQTT_PORT = 1883;
-const std::string MQTT_USER =
-    ""; // ANPASSEN: MQTT-Benutzername, falls erforderlich
-const std::string MQTT_PASSWORD =
-    ""; // ANPASSEN: MQTT-Passwort, falls erforderlich
-const std::string MQTT_TOPIC =
-    "public/mbed/teemaschine/cmd"; // ANPASSEN: MQTT-Topic
-std::string mqtt_client_name =
-    "esp8266_01"; // ANPASSEN: Client Name, womit diese app sich bei MQTT-Server
-                  // identifiziert
+// ANPASSEN: MQTT-Benutzername, falls erforderlich
+const std::string MQTT_USER = "";
+// ANPASSEN: MQTT-Passwort, falls erforderlich
+const std::string MQTT_PASSWORD = "";
+// ANPASSEN: MQTT-Topic
+const std::string MQTT_TOPIC = "public/mbed/teemaschine/cmd";
+// ANPASSEN: Client Name, womit diese app sich bei MQTT-Server identifiziert
+std::string mqtt_client_name = "esp8266_01";
 const MQTT::QoS MQTT_SUB_QOS = MQTT::QOS2;
 MQTTPacket_connectData MQTT_CONNECTION = MQTTPacket_connectData_initializer;
 unsigned int mqtt_messages_received = 0;
@@ -51,15 +48,7 @@ unsigned int anzahl_fehler = 0;
 void check_mqtt_message();
 void process_incoming_mqtt_message(MQTT::MessageData &md);
 bool ist_befehl(string befehl);
-typedef int (*Step)();
-int execute_step(Step what_step, int max_attempts = 5);
-int connect_to_wifi();
-int do_dns_lookup();
 int initialize_mqtt_client();
-void show_mqtt_options();
-int connect_to_mqtt_server();
-int subscribe_to_mqtt_topic();
-void restart_controller();
 
 void check_mqtt_message() {
   while (true) {
@@ -75,87 +64,16 @@ void process_incoming_mqtt_message(MQTT::MessageData &md) {
 
   std::string mqtt_message = (char *)message.payload;
   mqtt_message = mqtt_message.substr(0, msg_length);
-  if (!ist_befehl(mqtt_message)) {
-    LOG(MessageType::INFO, "MSG RCVD: {%d, %d, %s}", mqtt_messages_received,
-        msg_length, mqtt_message.c_str());
-  }
-}
-
-/*
-    This function executes a given function and examine its return value
-    and keeps executing the function until a return value of 0 is received
-    or max_attempts have been made
-    the function name to execute is passed as parameter
-    parameter function must have a return type of int
-*/
-
-typedef int (*Step)();
-int execute_step(Step what_step, int max_attempts) {
-  int attempts = 0;
-  int status = -1;
-  if (!critical_failure) {
-    while (status != 0 && attempts < max_attempts) {
-      status = what_step();
-      // DEBUG_LOG(0,".");
-      ThisThread::sleep_for(SLEEP_TIME_BETWEEN_INIT_STEPS);
-      attempts++;
-    }
-  }
-
-  // ThisThread::sleep_for(SLEEP_TIME_BETWEEN_STEPS);
-  // if critical failure has not occured and status is not 0/OK
-  if (status != 0 && !critical_failure) {
-    critical_failure = true;
-  }
-  return status;
-}
-
-int connect_to_wifi() {
-
-  int status = -1;
-  // WLAN Verbindung herstellen
-  status = wifi.connect(MBED_CONF_NSAPI_DEFAULT_WIFI_SSID,
-                        MBED_CONF_NSAPI_DEFAULT_WIFI_PASSWORD,
-                        NSAPI_SECURITY_WPA_WPA2);
-  if (status == 0) {
-    wifi.set_as_default();
-    LOG(MessageType::INFO, "WLAN Verbindung hergestellt");
-    wifi.get_ip_address(&socket_address);
-    LOG(MessageType::INFO, "IP Adresse: %s", socket_address.get_ip_address());
-  } else {
-    LOG(MessageType::ERROR,
-        "WLAN Verbindung konnte nicht hergestellt werden, Status: %d", status);
-    return status;
-  }
-  return status;
-}
-
-int do_dns_lookup() {
-  // MQTT Server DNS Lookup
-  int status = -1;
-  status = wifi.gethostbyname(MQTT_SERVER.c_str(), &socket_address);
-  if (status == 0) {
-    LOG(MessageType::INFO, "DNS Query Result of [%s]: %s", MQTT_SERVER.c_str(),
-        socket_address.get_ip_address());
-  } else {
-    LOG(MessageType::ERROR, "DNS Query failed for [%s], status: %d",
-        MQTT_SERVER.c_str(), status);
-    return status;
-  }
-  return status;
+  ist_befehl(mqtt_message);
 }
 
 int initialize_mqtt_client() {
   // MQTT Client Initialisierung
   char *client_id = (char *)mqtt_client_name.c_str();
-
   if (MQTT_CONNECTION.clientID.cstring != client_id) {
-
     MQTT_CONNECTION.MQTTVersion = 3;
     MQTT_CONNECTION.struct_version = 0;
     MQTT_CONNECTION.clientID.cstring = client_id;
-
-    // MQTT_CONNECTION.cleansession = 1;
     if (strlen(MQTT_USER.c_str()) > 1) {
       char *user = (char *)MQTT_USER.c_str();
       char *pass = (char *)MQTT_PASSWORD.c_str();
@@ -166,88 +84,24 @@ int initialize_mqtt_client() {
   return 0;
 }
 
-void show_mqtt_options() {
-  LOG("");
-  LOG("Client Name: %s", MQTT_CONNECTION.clientID.cstring);
-  LOG("Cleansession: %d", MQTT_CONNECTION.cleansession);
-  LOG("Mqtt Version: %d", MQTT_CONNECTION.MQTTVersion);
-  LOG("Mqtt Struct Version: %d", MQTT_CONNECTION.struct_version);
-  LOG("Mqtt User: %s", MQTT_CONNECTION.username.cstring);
-  LOG("Mqtt Pass: %s", MQTT_CONNECTION.password.cstring);
-  LOG("");
-}
-
-int connect_to_mqtt_server() {
-  // MQTT Client Verbindung herstellen
-  int status = -1;
-  status = mqttNet.connect(MQTT_SERVER.c_str(),
-                           MQTT_PORT); // tcp_socket.connect(socket_address);
-  status = mqtt_client.connect(MQTT_CONNECTION);
-  if (status == 0) {
-    LOG(MessageType::INFO, "MQTT Client Verbindung hergestellt");
-  } else {
-    LOG(MessageType::ERROR,
-        "MQTT Client Verbindung konnte nicht hergestellt werden, Status: %d",
-        status);
-    return status;
-  }
-  return status;
-}
-
-int subscribe_to_mqtt_topic() {
-  // MQTT Topic Abonieren
-  int status = mqtt_client.subscribe(MQTT_TOPIC.c_str(), MQTT_SUB_QOS,
-                                     process_incoming_mqtt_message);
-  if (status == 0) {
-    LOG(MessageType::INFO, "MQTT Topic [%s] abonniert", MQTT_TOPIC.c_str());
-  } else {
-    LOG(MessageType::ERROR,
-        "MQTT Topic konnte nicht abonniert werden, Status: %d", status);
-    return status;
-  }
-  return status;
-}
-
 void restart_controller() { NVIC_SystemReset(); }
 
 int init() {
-
-  LOG(MessageType::INFO, "Setup started...");
   int status = 0;
-
-  LOG("Connecting to Wifi");
-  status = execute_step(connect_to_wifi, 10);
-  if (status != 0)
-    return status;
-
-  LOG("Resolving Mqtt-Host to IP-Address");
-  status = execute_step(do_dns_lookup);
-  if (status != 0)
-    return status;
-
-  LOG("Setting Mqtt-Connection Parameters");
-  status = execute_step(initialize_mqtt_client);
-  if (status != 0)
-    return status;
-  // show_mqtt_options();
-
-  LOG("Connecting to Mqtt Server");
-  status = execute_step(connect_to_mqtt_server, 1);
-  if (status != 0)
-    return status;
-
-  LOG("Subscribing to Mqtt-Topic: %s", MQTT_TOPIC.c_str());
-  status = execute_step(subscribe_to_mqtt_topic);
-  if (status != 0)
-    return status;
-
+  status = wifi.connect(MBED_CONF_NSAPI_DEFAULT_WIFI_SSID,
+                        MBED_CONF_NSAPI_DEFAULT_WIFI_PASSWORD,
+                        NSAPI_SECURITY_WPA_WPA2);
+  ThisThread::sleep_for(4000ms); // vier sekunden
+  status = wifi.gethostbyname(MQTT_SERVER.c_str(), &socket_address);
+  ThisThread::sleep_for(SLEEP_TIME_BETWEEN_INIT_STEPS);
+  initialize_mqtt_client();
+  ThisThread::sleep_for(SLEEP_TIME_BETWEEN_INIT_STEPS);
+  status = mqttNet.connect(MQTT_SERVER.c_str(), MQTT_PORT);
+  ThisThread::sleep_for(SLEEP_TIME_BETWEEN_INIT_STEPS);
+  status = mqtt_client.connect(MQTT_CONNECTION);
+  status = mqtt_client.subscribe(MQTT_TOPIC.c_str(), MQTT_SUB_QOS,
+                                 process_incoming_mqtt_message);
   /******************************************************************
-  ##################################################################
-  ##################################################################
-  ##################################################################
-  ##################################################################
-  ##################################################################
-  ##################################################################
   ##################################################################
   ##################################################################
   CODE BIS HIER IST WICHTIG,
@@ -267,7 +121,7 @@ lcd tm_lcd;
 bool lcd_aktiv = true;
 
 // Enum
-enum COMMANDS{
+enum COMMANDS {
   STOP,
   MACH_TEE,
   TB_UNTEN,
@@ -279,7 +133,7 @@ enum COMMANDS{
   FAHR_LINKS,
   FAHR_OBEN,
   FAHR_UNTEN,
-  KALIBRIEREN  
+  KALIBRIEREN
 };
 
 // std::map<std::string, COMMANDS> command_map;
@@ -302,7 +156,7 @@ int motorlauf[] = {0b0001, 0b0011, 0b0010, 0b0110,
     unsigned int var_b = 0 - 1;
     - Dezimal Wert: var_b = 4294967295;
     - in Binär es ist: 11111111 11111111 11111111 11111111
-  
+
   aber weil Herr Kohler es so sagt!, nehmen wir unsigned int für motor_pos
 */
 unsigned int position_motor_teebeutel = 0;
@@ -359,10 +213,9 @@ int main() {
 
   while (true) {
 
-    if(zeige_motor_pos){
-      LOG(MessageType::DEBUG, "%d, %d", 
-        position_motor_teebeutel,
-        position_motor_stand);
+    if (zeige_motor_pos) {
+      LOG(MessageType::DEBUG, "%d, %d", position_motor_teebeutel,
+          position_motor_stand);
     }
 
     ThisThread::sleep_for(500ms);
@@ -384,77 +237,80 @@ int main() {
 bool ist_befehl(string befehl) {
   // die letzte befehl von mqtt-server wird nachdem start geholt
   // die erste befehl ignorieren
-  if(erste_befehl){ erste_befehl = false; return true; }
+  if (erste_befehl) {
+    erste_befehl = false;
+    return true;
+  }
 
   // für jede gewünschte befehl, eine neue if/else if block hinzufügen
   // in der if/else if block, eure Function aufrufen und dann return true machen
   if (befehl == "STOP") {
-    
+
     LOG(MessageType::INFO, "STOP");
     show_on_lcd("STOP");
     return true;
 
   } else if (befehl == "MACH_TEE") {
-    
+
     LOG(MessageType::INFO, "MACH_TEE");
     show_on_lcd("MACH_TEE");
     mache_tee();
     return true;
 
   } else if (befehl == "TB_UNTEN") {
-    
+
     LOG(MessageType::INFO, "TB_UNTEN");
     show_on_lcd("TB_UNTEN");
     fahr_teebeutel_unten();
     return true;
 
   } else if (befehl == "TB_HOCH") {
-    
+
     LOG(MessageType::INFO, "TB_HOCH");
     show_on_lcd("TB_HOCH");
     fahr_teebeutel_hoch();
     return true;
 
   } else if (befehl == "TB_SHAKE") {
-    
+
     LOG(MessageType::INFO, "TB_SHAKE");
     show_on_lcd("TB_SHAKE");
     shake_teebeutel();
     return true;
 
   } else if (befehl == "STAND_WEGWERF") {
-    
+
     LOG(MessageType::INFO, "STAND_WEGWERF");
     show_on_lcd("STAND_WEGWERF");
     fahr_stand_zu_wegwerf_pos();
     return true;
 
   } else if (befehl == "STAND_HOME") {
-    
+
     LOG(MessageType::INFO, "STAND_HOME");
     show_on_lcd("STAND_HOME");
     fahr_stand_home();
     return true;
   } else if (befehl == "FAHR_RECHTS") {
-    
+
     LOG(MessageType::INFO, "FAHR_RECHTS");
     show_on_lcd("FAHR_RECHTS");
     fahr_rechts();
     return true;
   } else if (befehl == "FAHR_LINKS") {
-    
+
     LOG(MessageType::INFO, "FAHR_LINKS");
     show_on_lcd("FAHR_LINKS");
     fahr_links();
     return true;
   } else if (befehl == "FAHR_OBEN") {
-    
+
     LOG(MessageType::INFO, "FAHR_OBEN");
     show_on_lcd("FAHR_OBEN");
     fahr_oben();
     return true;
   } else if (befehl == "FAHR_UNTEN") {
-    
+
     LOG(MessageType::INFO, "FAHR_UNTEN");
     show_on_lcd("FAHR_UNTEN");
     fahr_unten();
@@ -519,11 +375,15 @@ void fahr_teebeutel_hoch() { // teebeutal_hoch: 0
 
 void shake_teebeutel() {
   // der stand muss auf home-pos sein
-  if(position_motor_stand != stand_pos_home) { return; }
+  if (position_motor_stand != stand_pos_home) {
+    return;
+  }
   // teebeutel muss unten sein
-  if(position_motor_teebeutel != teebeutel_unten) { return; }
+  if (position_motor_teebeutel != teebeutel_unten) {
+    return;
+  }
 
-  while ((int)position_motor_teebeutel < (teebeutel_unten + teebeutel_shake) ) {
+  while ((int)position_motor_teebeutel < (teebeutel_unten + teebeutel_shake)) {
     position_motor_teebeutel++;
     ThisThread::sleep_for(MOTOR_SPEED);
     motor_teebeutel = motorlauf[position_motor_teebeutel % 8]
@@ -566,38 +426,38 @@ void mache_tee() {
 }
 
 void fahr_rechts() {
-  for(int i=0; i < step_size;i++){
+  for (int i = 0; i < step_size; i++) {
     position_motor_stand++;
     motor_stand = motorlauf[position_motor_stand % 8]
                   << motor_stand_verschiebung;
-  ThisThread::sleep_for(MOTOR_SPEED);
+    ThisThread::sleep_for(MOTOR_SPEED);
   }
   show_on_lcd("Fertig");
 }
 void fahr_links() {
-  for(int i=0; i < step_size;i++){
+  for (int i = 0; i < step_size; i++) {
     position_motor_stand--;
     motor_stand = motorlauf[position_motor_stand % 8]
                   << motor_stand_verschiebung;
-  ThisThread::sleep_for(MOTOR_SPEED);
+    ThisThread::sleep_for(MOTOR_SPEED);
   }
   show_on_lcd("Fertig");
 }
 void fahr_oben() {
-for(int i=0; i < step_size;i++){
+  for (int i = 0; i < step_size; i++) {
     position_motor_teebeutel++;
     motor_teebeutel = motorlauf[position_motor_teebeutel % 8]
                       << motor_teebeutel_verschiebung;
-  ThisThread::sleep_for(MOTOR_SPEED);
-  } 
-  show_on_lcd("Fertig"); 
+    ThisThread::sleep_for(MOTOR_SPEED);
+  }
+  show_on_lcd("Fertig");
 }
 void fahr_unten() {
-for(int i=0; i < step_size;i++){
+  for (int i = 0; i < step_size; i++) {
     position_motor_teebeutel--;
     motor_teebeutel = motorlauf[position_motor_teebeutel % 8]
                       << motor_teebeutel_verschiebung;
-  ThisThread::sleep_for(MOTOR_SPEED);
+    ThisThread::sleep_for(MOTOR_SPEED);
   }
   show_on_lcd("Fertig");
 }
@@ -606,5 +466,4 @@ void kalibrieren() {
   position_motor_stand = 0;
   position_motor_teebeutel = 0;
   show_on_lcd("Fertig");
-  
 }
